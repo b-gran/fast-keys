4b5e04ca1a6251cb5b35a4db806a2e2e10d37ca5
b-gran <bill@billgranfield.com>
2018-05-15 21:24:46 +0100

    Add initial tests and source

--- src/index.js
+++ src/index.js
@@ -1,1 +1,84 @@
-export default () => 'Welcome to fast-keys'
+export default function K (object) {
+  function isEnumberableNamedStringKey (key) {
+    return typeof key === 'string' && object.hasOwnProperty(key)
+  }
+
+  return {
+    get length () {
+      let length = 0
+      for (const key in object) {
+        if (!isEnumberableNamedStringKey(key)) {
+          continue
+        }
+        length += 1
+      }
+      return length
+    },
+    some: iteratee => {
+      for (const key in object) {
+        if (!isEnumberableNamedStringKey(key)) {
+          continue
+        }
+
+        if (iteratee(key)) {
+          return true
+        }
+      }
+      return false
+    },
+    map: iteratee => {
+      const result = []
+      for (const key in object) {
+        if (!isEnumberableNamedStringKey(key)) {
+          continue
+        }
+
+        result.push(iteratee(key))
+      }
+      return result
+    },
+    toSet: () => {
+      const set = new Set()
+      for (const key in object) {
+        if (!isEnumberableNamedStringKey(key)) {
+          continue
+        }
+        set.add(key)
+      }
+      return set
+    },
+    every: iteratee => {
+      for (const key in object) {
+        if (!isEnumberableNamedStringKey(key)) {
+          continue
+        }
+
+        if (!iteratee(key)) {
+          return false
+        }
+      }
+      return true
+    },
+    filter: iteratee => {
+      const filteredElements = []
+      for (const key in object) {
+        if (!isEnumberableNamedStringKey(key)) {
+          continue
+        }
+
+        if (iteratee(key)) {
+          filteredElements.push(key)
+        }
+      }
+      return filteredElements
+    },
+    forEach: iteratee => {
+      for (const key in object) {
+        if (!isEnumberableNamedStringKey(key)) {
+          continue
+        }
+        iteratee(object[key])
+      }
+    },
+  }
+}
