139c661001bcbd473d1548bf8e3cabba74fe320c
b-gran <bill@billgranfield.com>
2018-05-19 12:39:09 +0100

    Use a class in K() instead of a plain object.
    This change signifcantly improves performance (about 100x).
    
    The original version was creating a new object each time K() was called.
    This object wasn't a class, so K() wasn't taking advantage of V8's
    optimizations for creating new instaces of classes.

--- src/index.js
+++ src/index.js
@@ -1,100 +1,119 @@
+// It's important to use a class here to take advantage of V8's optimizations for classes.
+// If a plain object is created by K, the performance is up to 100x worse.
+class FastKeys {
+  constructor (object) {
+    this.object = object
+  }
+
+  get length () {
+    const object = this.object
+    let length = 0
+    for (const key in object) {
+      if (typeof key !== 'string' || !object.hasOwnProperty(key)) {
+        continue
+      }
+      length += 1
+    }
+    return length
+  }
+
+  some (iteratee) {
+    const object = this.object
+    for (const key in object) {
+      if (typeof key !== 'string' || !object.hasOwnProperty(key)) {
+        continue
+      }
+
+      if (iteratee(key)) {
+        return true
+      }
+    }
+    return false
+  }
+
+  map (iteratee) {
+    const object = this.object
+    const result = []
+    for (const key in object) {
+      if (typeof key !== 'string' || !object.hasOwnProperty(key)) {
+        continue
+      }
+
+      result.push(iteratee(key))
+    }
+    return result
+  }
+
+  toSet () {
+    const object = this.object
+    const set = new Set()
+    for (const key in object) {
+      if (typeof key !== 'string' || !object.hasOwnProperty(key)) {
+        continue
+      }
+      set.add(key)
+    }
+    return set
+  }
+
+  every (iteratee) {
+    const object = this.object
+    for (const key in object) {
+      if (typeof key !== 'string' || !object.hasOwnProperty(key)) {
+        continue
+      }
+
+      if (!iteratee(key)) {
+        return false
+      }
+    }
+    return true
+  }
+
+  filter (iteratee) {
+    const object = this.object
+    const filteredElements = []
+    for (const key in object) {
+      if (typeof key !== 'string' || !object.hasOwnProperty(key)) {
+        continue
+      }
+
+      if (iteratee(key)) {
+        filteredElements.push(key)
+      }
+    }
+    return filteredElements
+  }
+
+  forEach (iteratee) {
+    const object = this.object
+    for (const key in object) {
+      if (typeof key !== 'string' || !object.hasOwnProperty(key)) {
+        continue
+      }
+      iteratee(key)
+    }
+  }
+
+  find (predicate) {
+    const object = this.object
+    for (const key in object) {
+      if (typeof key !== 'string' || !object.hasOwnProperty(key)) {
+        continue
+      }
+
+      if (predicate(key)) {
+        return key
+      }
+    }
+    return undefined
+  }
+}
+
 export default function K (object) {
   if (object === null || object === undefined) {
     throw new TypeError('Cannot convert undefined or null to object')
   }
 
-  return {
-    get length () {
-      let length = 0
-      for (const key in object) {
-        if (!isEnumberableNamedStringKey(key, object)) {
-          continue
-        }
-        length += 1
-      }
-      return length
-    },
-    some: iteratee => {
-      for (const key in object) {
-        if (!isEnumberableNamedStringKey(key, object)) {
-          continue
-        }
-
-        if (iteratee(key)) {
-          return true
-        }
-      }
-      return false
-    },
-    map: iteratee => {
-      const result = []
-      for (const key in object) {
-        if (!isEnumberableNamedStringKey(key, object)) {
-          continue
-        }
-
-        result.push(iteratee(key))
-      }
-      return result
-    },
-    toSet: () => {
-      const set = new Set()
-      for (const key in object) {
-        if (!isEnumberableNamedStringKey(key, object)) {
-          continue
-        }
-        set.add(key)
-      }
-      return set
-    },
-    every: iteratee => {
-      for (const key in object) {
-        if (!isEnumberableNamedStringKey(key, object)) {
-          continue
-        }
-
-        if (!iteratee(key)) {
-          return false
-        }
-      }
-      return true
-    },
-    filter: iteratee => {
-      const filteredElements = []
-      for (const key in object) {
-        if (!isEnumberableNamedStringKey(key, object)) {
-          continue
-        }
-
-        if (iteratee(key)) {
-          filteredElements.push(key)
-        }
-      }
-      return filteredElements
-    },
-    forEach: iteratee => {
-      for (const key in object) {
-        if (!isEnumberableNamedStringKey(key, object)) {
-          continue
-        }
-        iteratee(key)
-      }
-    },
-    find: predicate => {
-      for (const key in object) {
-        if (!isEnumberableNamedStringKey(key, object)) {
-          continue
-        }
-
-        if (predicate(key)) {
-          return key
-        }
-      }
-      return undefined
-    }
-  }
-}
-
-function isEnumberableNamedStringKey (key, object) {
-  return typeof key === 'string' && object.hasOwnProperty(key)
+  return new FastKeys(object)
 }
